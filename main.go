4cb8dcd1384e9d74b330688cc4c40fb8a6fbb96c
Mehrab Rahman <mehrab.rahman@revature.com>
2019-10-01 11:50:30 -0500

    locale feature added

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, Go!\n")
+	var locale string = "fr"
+	var greeting string
+
+	if locale == "en" {
+		greeting = "Hello"
+	} else if locale == "es" {
+		greeting = "Hola"
+	} else if locale == "de" {
+		greeting = "Guten Tag"
+	} else {
+		greeting = "Yo"
+	}
+
+	fmt.Printf(greeting + ", Go!\n")
 }
