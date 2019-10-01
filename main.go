7f184f2b2e1e24b6dcbd8990edc386d1ac05c11d
Mehrab Rahman <mehrab.rahman@revature.com>
2019-10-01 12:01:35 -0500

    Array feature added

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	var locale string = "fr"
-	var greeting string
+	var locale, greeting string
+	var languages = [4]string{"en", "es", "de", "fr"}
+	locale = languages[1]
 
-	if locale == "en" {
+	/*
+		if locale == "en" {
+			greeting = "Hello"
+		} else if locale == "es" {
+			greeting = "Hola"
+		} else if locale == "de" {
+			greeting = "Guten Tag"
+		} else {
+			greeting = "Yo"
+		}
+	*/
+
+	switch locale {
+	case "en":
 		greeting = "Hello"
-	} else if locale == "es" {
+	case "es":
 		greeting = "Hola"
-	} else if locale == "de" {
+	case "de":
 		greeting = "Guten Tag"
-	} else {
+	case "fr":
+		greeting = "Bonjour"
+	default:
 		greeting = "Yo"
 	}
 
