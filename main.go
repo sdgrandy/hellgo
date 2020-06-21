8deefeab88784fab4a9d1bfe0558c0cb1227a5ee
Seth Grandy <sgrand051@comcast.com>
2020-06-21 02:34:50 -0400

    103

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	pw = replace(pw)
 	fmt.Println("password: ", pw)
 
-	replace("[a, d, m, i, n]")
+	replace("[g, u, e, s, s]")
 	var locale, greeting string
 	var languages = [4]string{"en", "es", "de", "fr"}
 	locale = languages[1]
